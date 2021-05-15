f9637697239b991438332569fe53407eec078e8b
Michael McLoughlin <mmcloughlin@gmail.com>
2021-05-14 23:36:51 -0700

    printer: new package

---
+++ printer/printer.go
@@ -0,0 +1,186 @@
+// Package printer implements printing of acc AST nodes.
+package printer
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/mmcloughlin/ssarules/ast"
+	"github.com/mmcloughlin/ssarules/internal/errutil"
+)
+
+// Bytes prints the AST and returns resulting bytes.
+func Bytes(n ast.Node) ([]byte, error) {
+	var buf bytes.Buffer
+	if err := Fprint(&buf, n); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+// Print an AST node to standard out.
+func Print(n ast.Node) error {
+	return Fprint(os.Stdout, n)
+}
+
+// Fprint writes the AST node n to w.
+func Fprint(w io.Writer, n ast.Node) error {
+	p := newprinter(w)
+	p.node(n)
+	return p.err
+}
+
+type printer struct {
+	w   io.Writer
+	err error
+}
+
+func newprinter(w io.Writer) *printer {
+	return &printer{w: w}
+}
+
+func (p *printer) node(node ast.Node) {
+	switch n := node.(type) {
+	case *ast.File:
+		for _, r := range n.Rules {
+			p.rule(r)
+			p.printf("\n")
+		}
+
+	case *ast.Rule:
+		p.rule(n)
+
+	case ast.Value:
+		p.value(n)
+
+	case ast.Op:
+		p.op(n)
+
+	default:
+		p.seterror(errutil.UnexpectedType(node))
+	}
+}
+
+func (p *printer) rule(r *ast.Rule) {
+	// Match
+	p.value(r.Match)
+
+	// Conditions (optional)
+	for _, c := range r.Conditions {
+		p.printf(" && %s", c)
+	}
+
+	// Deduction symbol
+	p.printf(" => ")
+
+	// Block (optional)
+	if r.Block != "" {
+		p.printf("@%s ", r.Block)
+	}
+
+	// Result
+	p.value(r.Result)
+}
+
+func (p *printer) value(val ast.Value) {
+	switch v := val.(type) {
+	case *ast.SExpr:
+		p.sexpr(v)
+	case ast.Expr, ast.Variable:
+		p.printf("%s", v)
+	default:
+		p.seterror(errutil.UnexpectedType(val))
+	}
+}
+
+func (p *printer) sexpr(s *ast.SExpr) {
+	// Binding (optional)
+	if s.Binding != "" {
+		p.printf("%s:", s.Binding)
+	}
+
+	// Open
+	p.printf("(")
+
+	// Op
+	p.op(s.Op)
+
+	// Type (optional)
+	if s.Type != "" {
+		p.printf(" <%s>", s.Type)
+	}
+
+	// AuxInt (optional)
+	if s.AuxInt != "" {
+		p.printf(" [%s]", s.AuxInt)
+	}
+
+	// Aux (optional)
+	if s.Aux != "" {
+		p.printf(" {%s}", s.Aux)
+	}
+
+	// Args
+	for _, arg := range s.Args {
+		p.printf(" ")
+		p.value(arg)
+	}
+
+	// Ellipsis
+	if s.Ellipsis {
+		p.printf(" ...")
+	}
+
+	// Trailing
+	if s.Trailing {
+		p.printf(" ___")
+	}
+
+	// Close
+	p.printf(")")
+}
+
+func (p *printer) op(op ast.Op) {
+	switch o := op.(type) {
+	case ast.Opcode:
+		p.printf("%s", o)
+	case ast.OpcodeParts:
+		for _, part := range o {
+			p.oppart(part)
+		}
+	default:
+		p.seterror(errutil.UnexpectedType(op))
+	}
+}
+
+func (p *printer) oppart(oppart ast.OpPart) {
+	switch part := oppart.(type) {
+	case ast.Opcode:
+		p.printf("%s", part)
+	case ast.OpcodeAlt:
+		sep := '('
+		for _, c := range part {
+			p.printf("%c%s", sep, c)
+			sep = '|'
+		}
+		p.printf(")")
+	default:
+		p.seterror(errutil.UnexpectedType(oppart))
+	}
+}
+
+func (p *printer) printf(format string, args ...interface{}) {
+	if p.err != nil {
+		return
+	}
+	_, err := fmt.Fprintf(p.w, format, args...)
+	p.seterror(err)
+}
+
+func (p *printer) seterror(err error) {
+	if p.err == nil {
+		p.err = err
+	}
+}
---
+++ printer/printer_test.go
@@ -0,0 +1,80 @@
+package printer_test
+
+import (
+	"bytes"
+	"flag"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/mmcloughlin/ssarules/internal/test"
+	"github.com/mmcloughlin/ssarules/parse"
+	"github.com/mmcloughlin/ssarules/printer"
+)
+
+var golden = flag.Bool("golden", false, "write golden testdata files")
+
+func TestGolden(t *testing.T) {
+	test.Glob(t, "../internal/testdata/*.rules", func(t *testing.T, filename string) {
+		// Parse file.
+		f, err := parse.File(filename)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Print to buffer.
+		got, err := printer.Bytes(f)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Compare to golden file.
+		name := strings.TrimSuffix(filepath.Base(filename), filepath.Ext(filename))
+		goldenfile := filepath.Join("testdata", name+".golden")
+
+		if *golden {
+			if err := ioutil.WriteFile(goldenfile, got, 0644); err != nil {
+				t.Fatal(err)
+			}
+		}
+
+		expect, err := ioutil.ReadFile(goldenfile)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if diff := cmp.Diff(expect, got); diff != "" {
+			t.Fatalf("diff =\n%s", diff)
+		}
+	})
+}
+
+func TestRoundtrip(t *testing.T) {
+	test.Glob(t, "../internal/testdata/*.rules", func(t *testing.T, filename string) {
+		// Parse file.
+		f, err := parse.File(filename)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Print it.
+		buf := bytes.NewBuffer(nil)
+		if err := printer.Fprint(buf, f); err != nil {
+			t.Fatal(err)
+		}
+
+		// Parse it again.
+		f2, err := parse.Reader("buffer", buf)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Verify roundtrip.
+		if diff := cmp.Diff(f, f2); diff != "" {
+			t.Logf("diff = \n%s", diff)
+			t.Fatal("roundtrip failure")
+		}
+	})
+}
