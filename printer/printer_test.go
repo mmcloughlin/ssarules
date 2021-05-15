bcf3dce61ccadb324a5aa620011852347d2ea923
Michael McLoughlin <mmcloughlin@gmail.com>
2021-05-15 00:06:39 -0700

    lint

--- printer/printer_test.go
+++ printer/printer_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+
 	"github.com/mmcloughlin/ssarules/internal/test"
 	"github.com/mmcloughlin/ssarules/parse"
 	"github.com/mmcloughlin/ssarules/printer"
